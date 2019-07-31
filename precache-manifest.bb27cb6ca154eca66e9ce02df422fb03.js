cd5435956717a0238d805814ff87ce0664fdd14d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-31 13:16:45 +0000

    Deploy npoirey/npoirey.github.io to github.com/npoirey/npoirey.github.io.git:master

---
+++ precache-manifest.bb27cb6ca154eca66e9ce02df422fb03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9aef9bd35fe59b542291",
+    "url": "/css/app.9f02f9fc.css"
+  },
+  {
+    "revision": "9aef9bd35fe59b542291",
+    "url": "/js/app.7bca6fdf.js"
+  },
+  {
+    "revision": "e2cd68b988c52932eb5f",
+    "url": "/js/chunk-vendors.bd4abfc2.js"
+  },
+  {
+    "revision": "b70edccd8b825f4b227b9a2dc28da846",
+    "url": "/fonts/Montalban Bold.b70edccd.otf"
+  },
+  {
+    "revision": "6a2d830d2fbdaf15cfd069d2ee9f55ab",
+    "url": "/fonts/Montalban.6a2d830d.otf"
+  },
+  {
+    "revision": "dc8a0189f6cb410760f8a4f0f7abc8ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b13792d2936a2192a387539f8e64837d.js"
+  "/precache-manifest.bb27cb6ca154eca66e9ce02df422fb03.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "infinity-quick-dice"});
